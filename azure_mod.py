3f08838544adfaccad8830eb1d501d4da2b6fe7e
Aashish <aashishpanta0@gmail.com>
2024-07-18 15:44:06 -0600

    test

--- azure_mod.py
+++ azure_mod.py
@@ -9,7 +9,7 @@ from urllib.request import urlretrieve
 from bitstring import Bits, pack
 from datetime import date, timedelta
 import sys
-import OpenVisus as ov
+# import OpenVisus as ov
 base_date = date(1950, 1, 1)
 present_date = date(2015, 1, 1)
 last_date = date(2100, 12, 31)
@@ -114,6 +114,7 @@ def _get_cmip6_data(model, scenario, variable, start_date, end_date, lb, ub):
         start_iidx = (item_start - date(year, 1 , 1)).days
         end_iidx = (item_end - date(year, 1, 1)).days + 1
         result[start_gidx:end_gidx,:,:] = data[start_iidx:end_iidx,lb[0]:ub[0],lb[1]:ub[1]]
+    result=np.random.randint(10,20,(30,40))
     return(result)
 
 def query(name, version, lb, ub):
